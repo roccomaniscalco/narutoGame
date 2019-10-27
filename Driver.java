1b2d5390070f40f774d3978c58b9573000b74749
zaydenSnacks <57055064+zaydenSnacks@users.noreply.github.com>
2019-10-27 08:00:19 -0400

    Add files via upload

---
+++ Driver.java
@@ -0,0 +1,11 @@
+
+public class Driver
+{
+    public static void main(String[] args)
+    {
+        Card player1 = new Card("Naruto");
+        Card player2 = new Card("Sauske");
+        System.out.println(player1);
+    }
+
+}
