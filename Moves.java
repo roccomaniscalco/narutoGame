f903b52b2c6514106cdca5bfb443b499fbb5eb11
zaydenSnacks <57055064+zaydenSnacks@users.noreply.github.com>
2019-10-28 10:08:39 -0400

    Update Moves.java

--- Moves.java
+++ Moves.java
@@ -2,10 +2,29 @@
 import java.util.*;
 public class Moves
 {
+    /*
     private ArrayList<Integer> damage;
     private int healthGiv;
     private int chakraAway;
     private int chakraGiv;
-
+    */
+    
+    
+    // still learning this shit
+    public class narutoMoves extends Moves 
+    {
+        public narutoMoves()
+        {
+        
+        }
+    }
+    
+    public class sauskeMoves extends Moves
+    {
+        public sauskeMoves()
+        {
+            
+        }
+    }
 
 }
